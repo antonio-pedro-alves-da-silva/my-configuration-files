b0031f1114c4b242bfb453a1a4a1729077523dc1
antonio <antoniopedro5999@gmail.com>
2023-06-30 15:44:40 -0300

    update my_configurations.sh and configurações-do-lubuntu.txt

--- .my_configurations.sh
+++ .my_configurations.sh
@@ -1,13 +1,5 @@
-# setting the right keyboard
-
-# setxkbmap -model abnt2 -layout br
-
 # adding a new display
 
 xrandr --newmode "2048x1152_60.00"  197.00  2048 2184 2400 2752  1152 1155 1160 1195 -hsync +vsync
 xrandr --addmode eDP-1 "2048x1152_60.00"
-
-# this commands reloads the .xbindkeysrc and applies the new keybindings
-cd ~/
-
-xbindkeys -p
+xrandr -s "2048x1152_60.00"
