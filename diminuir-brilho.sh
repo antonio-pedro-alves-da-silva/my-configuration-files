21a39e0882cbac07c6db4dc76497589dc83370f5
antonio <antoniopedro5999@gmail.com>
2023-07-01 09:43:37 -0300

    update

---
+++ diminuir-brilho.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+current_brightness=$(xrandr --verbose | grep -m 1 -i brightness | awk '{print $2}')
+new_brightness=$(echo "$current_brightness - 0.1" | bc)
+xrandr --output eDP-1 --brightness $new_brightness
