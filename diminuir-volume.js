9504c7e5467875cad55e5b76965b5ca7c6e1517e
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2023-07-16 22:47:32 -0300

    added the set o the main audio

---
+++ diminuir-volume.js
@@ -0,0 +1,19 @@
+const { exec } = require("child_process");
+
+const command = "pactl list sinks";
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`erro ao executar o comando =>> ${err}`);
+  } else {
+    let arrSinks = stdout.match(/(?<=Sink #)\d+/gm);
+    // increasing the audio
+    let increase = `pactl set-sink-volume ${arrSinks[arrSinks.length - 1]} -10%`;
+    exec(increase, (error, stdout, stderr) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
+
