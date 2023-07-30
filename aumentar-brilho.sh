6cd314bcdadc455c5d1749d180e80947ebd2554f
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2023-07-29 22:38:40 -0300

    update

--- aumentar-brilho.sh
+++ aumentar-brilho.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 current_brightness=$(xrandr --verbose | grep -m 1 -i brightness | awk '{print $2}')
 new_brightness=$(echo "$current_brightness + 0.1" | bc)
-xrandr --output eDP-1 --brightness $new_brightness
+output_name=$(xrandr | grep " connected primary" | awk '{print $1}')
+xrandr --output $output_name --brightness $new_brightness
