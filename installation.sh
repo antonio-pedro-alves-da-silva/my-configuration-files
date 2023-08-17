f0286e14053695edabdd23b89c68f58601896aa6
Antonio Pedro <86069266+antonio-pedro-alves-da-silva@users.noreply.github.com>
2023-08-17 12:58:44 -0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
 
-sudo add-apt-repository ppa:apt-fast/stable
-sudo apt-get install apt-fast -y
+sudo su
+
+add-apt-repository ppa:apt-fast/stable
+apt-get install apt-fast -y
 
 #    essential packeages for the installation
-sudo apt-fast install git wget curl
+apt-fast install git wget curl
 
 #   personal packeages installation
-sudo apt-fast install hardinfo snapd ffmpeg pavucontrol blueman nodejs -y
-sudo apt-fast install i3 feh -y
+apt-fast install hardinfo snapd ffmpeg pavucontrol blueman nodejs -y
+apt-fast install i3 feh -y
 cd ~/Downloads
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq && sudo ./auto-cpufreq-installer
 sudo auto-cpufreq --install
@@ -24,27 +26,27 @@ cp ~/my-configuration-files/config ~/.config/i3/
 
 
 #     installation for working
-sudo apt-fast install code -y
-sudo apt-fast install tilix -y
+apt-fast install code -y
+apt-fast install tilix -y
 #php e apache
-sudo apt-fast install apache2 php -y
+apt-fast install apache2 php -y
 
 #mysql e workbench
-sudo apt-fast install mysql-server -y
-sudo  mysql_secure_installation
-sudo apt-get install mysql-workbench-community -y
+apt-fast install mysql-server -y
+mysql_secure_installation
+apt-get install mysql-workbench-community -y
 
-sudo apt-fast install libcups2 samba samba-common -y
+apt-fast install libcups2 samba samba-common -y
 
 #     zsh installation and configuration
-sudo apt-fast install zsh -y
+apt-fast install zsh -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
-sudo apt-fast install fonts-powerline -y
+apt-fast install fonts-powerline -y
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 ' 
